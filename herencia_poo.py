02b79bb2806f3bb4dce72d1957796c793510b930
Bob <your new email>
2024-08-07 09:23:26 -0500

    Cambios finales a herencia_poo

--- herencia_poo.py
+++ herencia_poo.py
@@ -1,5 +1,6 @@
 class Vehiculo:
     def __init__(self, marca, modelo, precio):
+        #Encapsulamiento
         self.marca = marca
         self.modelo = modelo
         self.precio = precio
@@ -12,9 +13,11 @@ class Vehiculo:
         else:
             print(f"El vehículo {self.marca}. no está disponible")
 
+    #Abstracción
     def verifica_disponible(self):
         return self.disponible
     
+    #Abstracción
     def obtener_precio(self):
         return self.precio
     
@@ -23,27 +26,33 @@ class Vehiculo:
     
     def detener_motores(self):
         raise NotImplementedError("Este método debe ser implementado por la subclase")
-    
+
+#Herencia       
 class Coche(Vehiculo):
+    #Polimorfismo
     def iniciar_motores(self):
         if not self.disponible:
             return f"El motor del coche {self.marca} está en marcha"
         else:
             return f"El coche de marca {self.marca} no está disponible"
-        
+    
+    #Polimorfismo
     def detener_motores(self):
         if self.disponible:
             return f"El motor del coche {self.marca} se ha detenido"
         else:
             return f"El coche {self.marca} no está disponible"
         
+#Herencia
 class Bicicleta(Vehiculo):
+    #Polimorfismo
     def iniciar_motores(self):
         if not self.disponible:
             return f"La bicicleta {self.marca} está en marcha"
         else:
             return f"La bicicleta {self.marca} no está disponible"
-        
+
+    #Polimorfismo
     def detener_motores(self):
         if self.disponible:
             return f"La bicicleta {self.marca} se ha detenido"
@@ -89,7 +98,7 @@ class Concesionaria:
 
     def añadir_vehiculos(self, vehiculo:Vehiculo):
         self.inventario.append(vehiculo)
-        print(f"El {vehiculo.brand} ha sido añadido al inventario")
+        print(f"El {vehiculo.marca} ha sido añadido al inventario")
 
     def registrar_clientes(self, cliente: Cliente):
         self.clientes.append(cliente)
@@ -100,3 +109,27 @@ class Concesionaria:
         for vehiculo in self.inventario:
             if vehiculo.verifica_disponible():
                 print(f"- {vehiculo.marca} por {vehiculo.obtener_precio()}")
+
+
+coche1 = Coche("Toyota", "Corolla", 2000)
+bicicleta1 = Bicicleta("Yamaha", "MT-07", 700)
+camion1 = Camion("Volvo", "FH16", 8000)
+
+cliente1 = Cliente("Carlos")
+
+concesionaria = Concesionaria()
+concesionaria.añadir_vehiculos(coche1)
+concesionaria.añadir_vehiculos(bicicleta1)
+concesionaria.añadir_vehiculos(camion1)
+
+#Mostrar vehículos disponibles
+concesionaria.mostrar_vehiculos_disponibles()
+
+#El cliente va a consultar un vehículo
+cliente1.consultar_vehiculo(coche1)
+
+#El cliente va a comprar un vehículo
+cliente1.comprar_vehiculo(coche1)
+
+#Mostrar vehículos disponibles
+concesionaria.mostrar_vehiculos_disponibles()
