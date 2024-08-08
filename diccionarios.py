1faee5d5bdae77068a56a6ccf92721dce331054a
Bob <your new email>
2024-08-08 16:44:52 -0500

    Añadí dos ejercicios más respecto a comprehensions

---
+++ diccionarios.py
@@ -0,0 +1,30 @@
+﻿#Ejercicios de Dictionary Comprehension 1
+
+diccionario = {}
+
+for i in range(1,11):
+    diccionario[i] = i * 2
+
+print(diccionario)
+
+diccionario2 = {i: i * 2 for i in range(1,5)}
+print(diccionario2)
+
+import random
+paises = ["México", "Argentina", "Colombia"]
+poblacion = {}
+for pais in paises:
+    poblacion[pais] = random.randint(1,100)
+
+print(poblacion)
+
+poblacion2 = {pais: random.randint(1,100) for pais in paises}
+print(poblacion2)
+
+nombres = ["Nicolas", "Freddy", "Eddy"]
+edades = [13, 19, 23]
+
+print(list(zip(nombres, edades)))
+
+diccionario_nuevo = {nombres : edades for (nombres, edades) in zip(nombres, edades)}
+print(diccionario_nuevo)
---
+++ diccionarios_condicionales.py
@@ -0,0 +1,18 @@
+﻿#Ejercicios de Dictionary Comprehension 2
+
+import random
+paises = ["México", "Argentina", "Colombia"]
+
+poblacion2 = {pais: random.randint(1,100) for pais in paises}
+print(poblacion2)
+
+#Aquí creo un nuevo diccionario
+resultado = { pais: poblacion for (pais, poblacion) in poblacion2.items() if poblacion > 30}
+print(resultado)
+
+#Diccionario que regresa únicamente las vocales de mi texto
+#y las transforma a mayúsculas
+
+texto = "Hola soy Erick"
+unico = { c: c.upper() for c in texto if c in "aeiou"}
+print(unico)
