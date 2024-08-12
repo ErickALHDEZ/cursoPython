4e493d53fa0f4ec9faac0628fc48a7d87c9cdd6c
Erick <erickalpuchehdez@gmail.com>
2024-08-11 20:37:50 -0500

    Ejercicio de Reduce

---
+++ reduce.py
@@ -0,0 +1,16 @@
+﻿import functools
+
+numbers = [1,2,3,4]
+
+#Una función declarada que realiza lo mismo que la lambda de aquí abajo
+def acumulador(counter, item):
+    print("contador:  ", counter)
+    print("item:  ", item)
+    return counter + item
+
+result = functools.reduce(acumulador, numbers )
+#Función lambda con la cual sumo los valores dentro de la lista de numbers
+#result = functools.reduce(lambda counter, item: counter + item, numbers )
+
+print(result)
+
