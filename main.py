fb1774d394ac50185db9e77e91b5dab3c961aa42
Erick <erickalpuchehdez@gmail.com>
2024-08-17 18:35:02 -0500

    Ejercicios platzi __init__.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-﻿
+﻿from packages.mod_1 import func_1, func_2
+import packages.mod_1
+from packages.mod_2 import func_3, func_4
+
+print(func_1())
+print(func_2())
+
+import packages
+print(packages.URL)
+print(packages.mod_1.func_1())
---
+++ packages/__init__.py
@@ -0,0 +1,6 @@
+﻿print("Se inició paquete")
+
+URL = "platzi.com"
+
+
+import packages.mod_1, packages.mod_2
